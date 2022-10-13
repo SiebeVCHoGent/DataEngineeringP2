bc86d1a03541f6d64a5922b603ccbe4769d4a7ef
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-10-13 16:20:07 +0200

    Omzet en balans toegevoegd

--- Scratches/all.py
+++ Scratches/all.py
@@ -20,7 +20,12 @@ except FileNotFoundError as E:
 
 
 def read_kmos(shuffle=False):
-    df = pd.read_csv("kmo_test_300.csv", sep=",")
+    df = pd.read_csv("kmos.csv", sep=",")
+    # replace omzet with n.b. with None
+    df['omzet'] = df['omzet'].replace('n.b.', None)
+    # remove all points from omzet and balanstotaal
+    df['omzet'] = df['omzet'].str.replace('.', '')
+    df['balanstotaal'] = df['balanstotaal'].str.replace('.', '')
     if shuffle:
         #shuffle dataframe
         df = df.sample(frac=1).reset_index(drop=True)
@@ -64,13 +69,14 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # clear all tables
-session.query(Jaarverslag).delete()
-session.query(Website).delete()
-session.query(Verslag).delete()
-session.query(Kmo).delete()
-session.query(Gemeente).delete()
-session.query(Sector).delete()
-session.commit()
+def clear_tables():
+    session.query(Jaarverslag).delete()
+    session.query(Website).delete()
+    session.query(Verslag).delete()
+    session.query(Kmo).delete()
+    session.query(Gemeente).delete()
+    session.query(Sector).delete()
+    session.commit()
 
 start = datetime.now()
 
@@ -128,7 +134,11 @@ def add_fully_kmo(kmo, get_website_data=False, scrape_nbb=False):
             Verslag.jaar == datetime.now().year).first()
     if verslag is None:
         verslag = Verslag(ondernemingsnummer=kmo['bvd_id'],
-                          jaar=datetime.now().year)
+                          jaar=datetime.now().year,
+                          omzet=kmo['omzet'],
+                          balanstotaal=kmo['balanstotaal'],
+                          aantalwerkenemers=kmo['werknemers']
+                          )
         session.add(verslag)
         verslag = session.query(Verslag).filter(
             Verslag.ondernemingsnummer == kmo['bvd_id']).filter(
@@ -154,11 +164,17 @@ def add_fully_kmo(kmo, get_website_data=False, scrape_nbb=False):
     session.add(website_to_add)
     session.commit()
 
+clear_tables()
 
-kmos = read_kmos()
-for idx, kmo in enumerate(kmos[:]):
-    add_fully_kmo(kmo, get_website_data=True, scrape_nbb=True)
-    print(f"kmo {idx+1}: {kmo['Naam']}")
-    #sleep(3)
+kmos = read_kmos(True)
+for idx, kmo in enumerate(kmos[:50]):
+    try:
+        add_fully_kmo(kmo, get_website_data=True, scrape_nbb=True)
+        print(f"kmo {idx+1}: {kmo['Naam']}")
+        sleep(3)
+    except Exception as e:
+        print(f"Exception adding kmo {idx+1}: {kmo['Naam']}")
+        print(e)
+        pass
 
 print(f"TIJD: {(datetime.now() - start).seconds} seconden")
